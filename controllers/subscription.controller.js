4f995f8dc2f7c6f824f0d24b0be879ed05ebc75b
Luis Rodriguez <rodriguezluis@soy.sena.edu.co>
2023-05-12 13:45:42 -0500

    Progreso en subscription routes and controller

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -9,13 +9,105 @@ class SubscriptionController {
 
   async findAll(req, res) {
     const data = await this.model.find({});
-    res.status(200).send({ data });
+    res.status(200).send({
+      code: 200,
+      data,
+    });
   }
 
   async findByEmail(req, res) {
-    const { email } = req?.params;
+    const { email } = req.params;
     const data = await this.model.find({ email });
-    res.status(200).send({ data });
+    res.status(200).send({
+      code: 200,
+      data,
+    });
+  }
+
+  save(req, res) {
+    const { email, start_date, end_date, status, price } = req.body;
+
+    const fields = {
+      email,
+      start_date,
+      end_date,
+      status,
+      price,
+    };
+
+    this.model.save(fields, (error) => {
+      if (!error) {
+        res.status(200).send({
+          code: 200,
+          message: "Subscription saved successfully!",
+        });
+      } else {
+        this.handleError(error);
+      }
+    });
+  }
+
+  update(req, res) {
+    const { email, start_date, end_date, status, price } = req.body;
+
+    const fields = {
+      email,
+      start_date,
+      end_date,
+      status,
+      price,
+    };
+
+    const fieldsFiltered = fields.filter((field) => field !== "");
+
+    this.model.updateOne({ email }, fieldsFiltered, (error) => {
+      if (!error) {
+        res.status(200).send({
+          code: 200,
+          message: "Subscription updated successfully!",
+        });
+      } else {
+        this.handleError(error);
+      }
+    });
+  }
+
+  updateStatus(req, res) {
+    const { email, status } = req.body;
+
+    this.model.updateOne({ email }, { status }, (error) => {
+      if (!error) {
+        res.status(200).send({
+          code: 200,
+          message: "Subscription updated successfully!",
+        });
+      } else {
+        this.handleError(error);
+      }
+    });
+  }
+
+  delete(req, res) {
+    const { email } = req.body;
+
+    this.model.deleteOne({ email }, (error) => {
+      if (!error) {
+        res.status(200).send({
+          code: 200,
+          message: "Subscription deleted successfully!",
+        });
+      } else {
+        this.handleError(error);
+      }
+    });
+  }
+
+  handleError(error) {
+    res.status(500).send({
+      code: 500,
+      message: "An error has ocurred, please try again.",
+      error,
+    });
   }
 }
 
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -7,7 +7,34 @@ const SubscriptionController = require("../controllers/subscription.controller")
 const SubscriptionModel = require("../models/subscription.model");
 const Subscription = new SubscriptionController(SubscriptionModel);
 
-router.get("/", (req, res) => Subscription.findAll(req, res));
-router.get("/:email", (req, res) => Subscription.findByEmail(req, res));
+// Get all subscription
+router.get("/", (req, res) => 
+  Subscription.findAll(req, res)
+);
+
+// Find subscription by email
+router.get("/search/:email", (req, res) => 
+  Subscription.findByEmail(req, res)
+);
+
+// Save subscription
+router.post("/save", (req, res) => 
+  Subscription.save(req, res)
+);
+
+// Admin Update
+router.put("/update", (req, res) => 
+  Subscription.update(req, res)
+);
+
+// User Update (status only)
+router.put("/update-status", (req, res) => 
+  Subscription.updateStatus(req, res)
+);
+
+// Delete
+router.delete("/delete", (req, res) => 
+  Subscription.delete(req, res)
+);
 
 module.exports = router;
