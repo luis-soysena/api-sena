6a9475b4ed88193466d3924e7e37dbadd362857b
Luis Rodriguez <rodriguezluis@soy.sena.edu.co>
2023-05-21 08:27:02 -0500

    Mejora en la validacion de la API key

--- middlewares/apiKey.middleware.js
+++ middlewares/apiKey.middleware.js
@@ -3,11 +3,21 @@
 class ApiKeyMiddleware {
   static check(req, res, next) {
     const apiKey = req.header("x-api-key");
+
+    if (!apiKey){
+      return next(
+        res.status(401).send({
+          code: 401,
+          message: "Unauthorized: No API key supplied.",
+        })
+      );
+    }
+
     if (apiKey !== process.env.API_KEY) {
       return next(
         res.status(401).send({
           code: 401,
-          message: "Unauthorized: No API Key supplied.",
+          message: "Unauthorized: Invalid API key.",
         })
       );
     }
